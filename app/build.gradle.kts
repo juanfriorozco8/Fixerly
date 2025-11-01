5c36687db2137c0ee1d04324f446b88e4adb7cf9
juanfriorozco8 <oro24647@uvg.edu.gt>
2025-11-01 17:30:39 -0600

    gradle actualizado para navegación

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // navegación
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -57,4 +60,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // navegación
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 }
